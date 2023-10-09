ccf9889487fed36a16ba299e7b5de34bfe129576
Pushkaraj <pushkaraj4444@gmail.com>
2023-10-09 19:51:48 +0530

    Banner Section changes 1

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,98 +1,117 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-// import { BLOCKS } from "@contentful/rich-text-types";
-// import client from "../pages/client";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import { graphql, useStaticQuery} from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const query = graphql`
+  query {
+    allContentfulBannerSection {
+      nodes {
+        title
+        description {
+          references {
+            gatsbyImageData
+          }
+          raw
+        }
+        backgroundImage {
+          id
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`
 
 const Banner = () => {
-//   const [bannerItems, setBannerItems] = useState([]);
-//   const [backgroundImage, setBackgroundImage] = useState("");
-//   const { slug } = useParams();
+  const [bannerItems, setBannerItems] = useState([]);
+  const [backgroundImage, setBackgroundImage] = useState("");
+  const { slug } = useParams();
+  const data = useStaticQuery(query);
 
 
-//  useEffect(() => {
-//     async function getMenuItems() {
-//       try {
-  
-//         const entries = await client.getEntries({
-//           content_type: "bannerSection",
-//           "sys.id": '4hURH4J5WPqHSjm3vABwxo',
-//         });
-//         setBannerItems(entries.items.reverse());
-//         console.log(entries)
-//         setBackgroundImage(
-//           entries.items[0]?.fields.backgroundImage?.fields.file.url || ""
-//         );
-//       } catch (error) {
-//         console.error("Error fetching menu items:", error);
-//       }
-//     }
-//     getMenuItems();
-//   }, [slug]);
+ useEffect(() => {
+    async function getMenuItems() {
+      try {
+        const entries = data.allContentfulBannerSection.nodes;
+        setBannerItems(entries.reverse());
+        console.log(entries);
+        setBackgroundImage(
+          entries[0]?.backgroundImage?.gatsbyImageData || ""
+        );
+      } catch (error) {
+        console.error("Error fetching menu items:", error);
+      }
+    }
+    getMenuItems();
+  }, [slug]);
 
-//   const renderCustomRichTextHeading = (node, children) => (
-//     <div className="custom-rich-text-heading">
-//       {children.map((child, index) => (
-//         <h1 key={index}>{child}</h1>
-//       ))}
-//     </div>
-//   );
+  // const renderCustomRichTextHeading = (node, children) => (
+  //   <div className="custom-rich-text-heading">
+  //     {children.map((child, index) => (
+  //       <h1 key={index}>{child}</h1>
+  //     ))}
+  //   </div>
+  // );
 
-//   const renderCustomRichTextSubheading = (node, children) => (
-//     <div className="custom-rich-text-subheading">
-//       {children.map((child, index) => (
-//         <h2 key={index}>{child}</h2>
-//       ))}
-//     </div>
-//   );
+  // const renderCustomRichTextSubheading = (node, children) => (
+  //   <div className="custom-rich-text-subheading">
+  //     {children.map((child, index) => (
+  //       <h2 key={index}>{child}</h2>
+  //     ))}
+  //   </div>
+  // );
 
-//   const headingRenderers = {
-//     [BLOCKS.HEADING_1]: renderCustomRichTextHeading,
-//     [BLOCKS.HEADING_2]: renderCustomRichTextSubheading,
-//   };
+  // const headingRenderers = {
+  //   [BLOCKS.HEADING_1]: renderCustomRichTextHeading,
+  //   [BLOCKS.HEADING_2]: renderCustomRichTextSubheading,
+  // };
 
-//   const renderRichText = (richText) => {
-//     if (richText) {
-//       return (
-//         <div className="custom-rich-text-container">
-//           {documentToReactComponents(richText, {
-//             renderNode: {
-//               ...headingRenderers,
-//               [BLOCKS.EMBEDDED_ASSET]: (node) => {
-//                 const { title, description, file } = node.data.target.fields;
-//                 const imageUrl = file.url;
-//                 const altText = description || title || "Image";
-//                 return (
-//                   <div className="custom-rich-text-block">
-//                     <img src={imageUrl} alt={altText} loading="lazy" />
-//                   </div>
-//                 );
-//               },
-//             },
-//           })}
-//         </div>
-//       );
-//     }
-//     return null;
-//   };
+  // const renderRichText = (richText) => {
+  //   if (richText) {
+  //     return (
+  //       <div className="custom-rich-text-container">
+  //         {documentToReactComponents(richText, {
+  //           renderNode: {
+  //             ...headingRenderers,
+  //             [BLOCKS.EMBEDDED_ASSET]: (node) => {
+  //               const {title} = node.title;
+  //               const {description} = node.description.raw;
+  //               const {imageUrl} = node.description.references.gatsbyImageData;
+  //               const altText = description || title || "Image";
+  //               return (
+  //                 <div className="custom-rich-text-block">
+  //                   <GatsbyImage image = {imageUrl} alt = {altText}/>
+  //                 </div>
+  //               );
+  //             },
+  //           },
+  //         })}
+  //       </div>
+  //     );
+  //   }
+  //   return null;
+  // };
 
 
   return (
     <section
-      // className="banner"
-      // style={{ backgroundImage: `url(${backgroundImage})` }}
+      className="banner"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      {/* <div className="background-overlay"></div>
+      <div className="background-overlay"></div>
       <div className="container">
       <div className="d-flex">
-        {bannerItems.map((item) => (
-          <React.Fragment key={item.sys.id}>
-            {renderRichText(item.fields.description)}
+        {bannerItems.map((entries) => (
+          <React.Fragment key={entries.id}>
+            <h1>{entries.title}</h1>
+            <GatsbyImage image = {entries.description.references.gatsbyImageData} alt={entries.id} />
           </React.Fragment>
         ))}
       </div>
-      </div> */}
-      <h1>banner</h1>
+      </div>
     </section>
   );
 };
