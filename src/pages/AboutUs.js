f513f4f8d68f24ff6f2d11421e628a26745a16c9
Yogiraj Patil <yogiraj10033921@gmail.com>
2023-10-18 19:09:52 +0530

    about us initial

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,15 +1,31 @@
 // About.js
 import React from "react";
+import Header from '../Components/Header';  
 import Banner from "../Components/Banner";
+import BasicComponent from "../Components/BasicComponent";
+import Philosophy from "../Components/Philosophy";
+import PhilosophyProgram from "../Components/PhilosophyProgram";
+import Director from "../Components/Director";
+import StayConnected from "../Components/StayConnected";
+import Footer from "../Components/Footer";
+
 
 const AboutUs = () => {
   const slug = "about-page-banner"; // Set the slug for the about page banner
   console.log("Slug for About Page:", slug);
   return (
     <>
+      <Header />
+      <Banner slug={slug} />
+      <BasicComponent/>
+      <Philosophy/>
+      <PhilosophyProgram/>
+      <Director/>
+      <StayConnected/>
+      <Footer/>
       {/* Other content */}
       {/* Other content */}
-      <h1>About page</h1>
+      {/* <h1>About page</h1> */}
     </>
   );
 };
